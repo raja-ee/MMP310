0002ad932523229897f4a57df05adc32f4b80b70
raja-ee <rhloney@gmail.com>
2020-09-09 19:08:00 -0400

    character update

--- project1/sketch.js
+++ project1/sketch.js
@@ -1,14 +1,54 @@
-let char;
+/* */
+
+var bobIdle;
+var bobRun;
+var bobX;
+var bobY;
+var bobSpeed = 4;
 
 function preload() {
-    
+    bobIdle = loadImage('assets/positive_idle.gif');
+    bobRun = loadImage('assets/positive_run.gif');
 }
 
 function setup() {
-	createCanvas(600,600);
+	createCanvas(600, 600);
+    bobX = width/2;
+    bobY = height/2;
+    imageMode(CENTER);
 }
 
 function draw() {
 	background('black');
 	noCursor();
+    
+    var bobIsRunning = false;
+
+	if (keyIsDown(RIGHT_ARROW)) {
+		bobX += bobSpeed;
+		bobIsRunning = true;
+	}
+
+	if (keyIsDown(LEFT_ARROW)) {
+		bobX -= bobSpeed;
+		bobIsRunning = true;
+	}
+
+	if (keyIsDown(UP_ARROW)) {
+		bobY -= bobSpeed;
+		bobIsRunning = true;
+	}
+
+	if (keyIsDown(DOWN_ARROW)) {
+		bobY += bobSpeed;
+		bobIsRunning = true;
+	}
+
+	/* draw character image */
+    
+	if (bobIsRunning) {
+		image(bobRun, bobX, bobY);
+	} else {
+		image(bobIdle, bobX, bobY);
+	}
 }
